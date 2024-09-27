7696fe622ccce3975da8ebeea6512830db8da1e1
Nikhil Kamath <nikhil.k123234@gmail.com>
2024-09-27 17:29:53 -0400

    Removing old AST

--- play/test/test_play.ml
+++ play/test/test_play.ml
@@ -1,37 +1,6 @@
 open Base.Result
 open Play
-open Play.Reduce
 open Play.Term
-
-let _ =
-  let open Play.Mexpression in
-  let m1 =
-    { dims = 2; shape = [ 5; 3 ]; elements = Base.List.range 0 (5 * 3) }
-  in
-  let m2 =
-    { dims = 2; shape = [ 3; 4 ]; elements = Base.List.range 0 (3 * 4) }
-  in
-  let m3 =
-    { dims = 2; shape = [ 3; 4 ]; elements = Base.List.range 0 (3 * 4) }
-  in
-  let m1_e = Matrix m1 in
-  let m2_e = Matrix m2 in
-  let m3_e = Matrix m3 in
-
-  assert (check m1_e = Ok (Shape m1.shape));
-  assert (check m2_e = Ok (Shape m2.shape));
-
-  let mx_e = Binop (Multiply, m1_e, m2_e) in
-  assert (check mx_e = Ok (Shape [ 5; 4 ]));
-
-  let mx_e2 = Scale (Scalar 2, mx_e) in
-  assert (check mx_e2 = Ok (Shape [ 5; 4 ]));
-
-  let mp_e = Binop (Add, m2_e, m3_e) in
-  assert (check mp_e = Ok (Shape m3.shape));
-
-  let mb_e = If (true, m2_e, m3_e) in
-  assert (check mb_e = Ok (Shape m3.shape))
 
 let _ =
   let open Play.Term in
